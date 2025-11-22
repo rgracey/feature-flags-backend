f95580ede5b014ca232029788164a44244278c21
Robert Gracey <70551819+rgracey@users.noreply.github.com>
2025-11-22 12:04:40 +1100

    chore: add /api prefix to routes

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('api', {
+    exclude: [{ path: 'health', method: RequestMethod.GET }],
+  });
+
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
