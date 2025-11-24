0e291f7f85af99f221b00ccb7b645229a323f3e5
Robert Gracey <70551819+rgracey@users.noreply.github.com>
2025-11-24 19:35:15 +1100

    chore: remove unused dependency

--- src/feature-flags/feature-flags.module.ts
+++ src/feature-flags/feature-flags.module.ts
@@ -4,7 +4,6 @@ import { FeatureFlagsService } from './services';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { FeatureFlag } from './entities';
 import { AuthenticationModule } from 'src/authentication/authentication.module';
-import { ProjectsModule } from 'src/projects';
 import { AuthorisationModule } from 'src/authorisation/authorisation.module';
 
 @Module({
@@ -12,7 +11,6 @@ import { AuthorisationModule } from 'src/authorisation/authorisation.module';
     MikroOrmModule.forFeature([FeatureFlag]),
     AuthenticationModule,
     AuthorisationModule,
-    ProjectsModule,
   ],
   controllers: [FeatureFlagsController],
   providers: [FeatureFlagsService]
